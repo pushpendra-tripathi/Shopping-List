3929511801052825e9975e375d77ca69bd4cb437
pushpendra-starlord <pushpendratripathi@outlook.com>
2020-02-28 23:56:40 +0530

    first commit

--- app/src/main/java/com/starlord/dailyshoppinglist/SplashScreenActivity.java
+++ app/src/main/java/com/starlord/dailyshoppinglist/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.starlord.dailyshoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent start = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(start);
+                finish();
+            }
+        },3000);
     }
 }
